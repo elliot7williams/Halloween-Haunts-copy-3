6188bdc3781cffc20549a0faef89c3af12f9d2a6
elliot7williams <58864955+elliot7williams@users.noreply.github.com>
2026-08-01 22:21:36 -0400

    Add Halloween Haunts copy 3/Halloween Haunts/Halloween_HauntsApp.swift

---
+++ Halloween Haunts copy 3/Halloween Haunts/Halloween_HauntsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Halloween_HauntsApp.swift
+//  Halloween Haunts
+//
+//  Created by Elliot Williams on 2025-06-20.
+//
+
+import SwiftUI
+
+@main
+struct Halloween_HauntsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HalloweenGame()
+        }
+    }
+}
