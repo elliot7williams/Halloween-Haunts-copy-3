882f9e73ba86bfb7548bbbffcce81855c870eeee
elliot7williams <58864955+elliot7williams@users.noreply.github.com>
2026-08-01 22:21:29 -0400

    Add Halloween Haunts copy 3/Halloween HauntsTests/Halloween_HauntsTests.swift

---
+++ Halloween Haunts copy 3/Halloween HauntsTests/Halloween_HauntsTests.swift
@@ -0,0 +1,17 @@
+//
+//  Halloween_HauntsTests.swift
+//  Halloween HauntsTests
+//
+//  Created by Elliot Williams on 2025-06-20.
+//
+
+import Testing
+@testable import Halloween_Haunts
+
+struct Halloween_HauntsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
